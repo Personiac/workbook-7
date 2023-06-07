04ccc3824a2a46c01740fc389dee9e7b4b9cbfff
Jonathan Tran <trandingtopics@gmail.com>
2023-06-07 11:31:59 -0400

    jank code, API test

--- exercises/miniProject/scripts/index.js
+++ exercises/miniProject/scripts/index.js
@@ -3,14 +3,17 @@
 const cityWeatherTblBody = document.querySelector("#weather-tbl-body");
 const cityDDL = document.querySelector("#city-DDL");
 
-function buildWeatherRow(city) {
+function buildWeatherRow(cityInfo, lat, long) {
   let row = cityWeatherTblBody.insertRow(-1);
 
-  row.insertCell(0).innerText = city.city;
-  row.insertCell(1).innerText = city.latitude;
-  row.insertCell(2).innerText = city.longitude;
-  row.insertCell(3).innerText = city.population;
-  row.insertCell(4).innerText = city.weather;
+  console.log(cityInfo);
+
+  row.insertCell(0).innerText = cityInfo.properties.relativeLocation.properties.city;
+  row.insertCell(1).innerText = cityInfo.properties.relativeLocation.properties.state;
+  row.insertCell(2).innerText = lat;
+  row.insertCell(3).innerText = long;
+//   row.insertCell(4).innerText = cityInfo.population;
+//   row.insertCell(5).innerText = cityInfo.weather;
 
   cityWeatherTblBody.appendChild(row);
 }
@@ -22,6 +25,8 @@ function loadCityDDL() {
   selectOption.value = " ";
   selectOption.textContent = "Select City...";
   cityDDL.appendChild(selectOption);
+  
+  cities.sort((a,b) => a.city.localeCompare(b.city));
 
   for (const cityProfile of cities) {
     let option = new Option(cityProfile.city, count);
@@ -32,24 +37,27 @@ function loadCityDDL() {
 
 loadCityDDL();
 
-function findCityWeather(cityProfile) {
-  let cityName = cityProfile.city;
-  let cityLat = cityProfile.latitude;
-  let cityLong = cityProfile.longitude;
+function displayCityDetails(cityIndex) {
+  clearTable();
+  let cityInfo = cities.find(function (city, index) {
+    return cityIndex == index;
+  });
 
+  let cityLat = cityInfo.latitude;
+  let cityLong = cityInfo.longitude;
+
+  console.log(`City: ${cityInfo.city}, Lat: ${cityLat}, Long: ${cityLong}`);
+  open(`https://api.weather.gov/points/${cityLat},${cityLong}`);
   fetch(`https://api.weather.gov/points/${cityLat},${cityLong}`)
-    .then((cityWeather) => cityWeather.json())
+    .then((cityDetails) => cityDetails.json())
     .then((content) => {
       return content;
     })
     .then((info) => {
-      const cityInfo = {
-        city: cityName,
-        latitude: cityLat,
-        longitude: cityLong,
-        population: cityProfile.population,
-        weather: info.properties.forecast
-      };
-      buildWeatherRow(cityInfo);
+      buildWeatherRow(info, cityLat, cityLong);
     });
+}
+
+function clearTable() {
+  cityWeatherTblBody.innerHTML = "";
 }
